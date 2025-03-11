f6f6256aabaf3e96b49ba517b26bd194c9694e76
James Pascoe <james.pascoe@bluwireless.com>
2025-03-11 08:19:18 +0000

    Code fix-ups for presentation in slides

--- sender_and_receivers_timer_example.cpp
+++ sender_and_receivers_timer_example.cpp
@@ -8,23 +8,23 @@ using namespace stdexec::tags;
 namespace asio = boost::asio;
 
 template <class Receiver>
-struct timer_operation {
+struct timer_op {
   asio::io_context& io;
-  int num_secs;
+  int n_secs;
   asio::steady_timer timer;
   Receiver rcvr;
 
-  static void callback(const boost::system::error_code& error, int num_secs, Receiver& rcvr)
+  static void callback(const boost::system::error_code& error, int n_secs, Receiver& rcvr)
   {
-    std::printf("Finished timer for %d seconds\n", num_secs);
+    std::printf("Finished timer for %d seconds\n", n_secs);
     if (error != boost::system::errc::success)
       stdexec::set_error(std::move(rcvr), error);
     else
       stdexec::set_value(std::move(rcvr), 0);
   }
 
-  STDEXEC_MEMFN_DECL(void start)(this timer_operation& self) noexcept {
-    self.timer.async_wait(boost::bind(callback, boost::asio::placeholders::error, self.num_secs, self.rcvr));
+  STDEXEC_MEMFN_DECL(void start)(this timer_op& self) noexcept {
+    self.timer.async_wait(boost::bind(callback, boost::asio::placeholders::error, self.n_secs, self.rcvr));
     std::printf("Running\n");
     self.io.run();
   }
@@ -39,23 +39,23 @@ struct timer_sender {
       stdexec::set_error_t(int)>;
 
   STDEXEC_MEMFN_DECL(auto connect)(this timer_sender self, stdexec::receiver auto rcvr) {
-    asio::steady_timer timer(self.io, asio::chrono::seconds(self.num_secs));
+    asio::steady_timer timer(self.io, asio::chrono::seconds(self.n_secs));
 
-    std::printf("setup done for %d seconds\n", self.num_secs);
-    return timer_operation{self.io, self.num_secs, std::move(timer), std::move(rcvr)};
+    std::printf("setup done for %d seconds\n", self.n_secs);
+    return timer_op{self.io, self.n_secs, std::move(timer), std::move(rcvr)};
   };
 
   asio::io_context& io;
-  int num_secs;
+  int n_secs;
 };
 
-timer_sender async_timer(asio::io_context& io, int num_secs) {
-  return {io, num_secs};
+timer_sender async_timer(asio::io_context& io, int n_secs) {
+  return {io, n_secs};
 }
 
 int main() {
-
   asio::io_context io;
+
   auto task_timer = async_timer(io, 5);
   auto ret = stdexec::sync_wait(task_timer).value();
 }
